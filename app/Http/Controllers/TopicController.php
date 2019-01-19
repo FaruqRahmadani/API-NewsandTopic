f3a2b5dce2095272d4b3e4ca173bbb0f2d53236e
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-19 23:59:10 +0700

    update topic data

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -21,4 +21,13 @@ class TopicController extends Controller
     if ($topic) return "Success";
     return "Error";
   }
+
+  public function update($id, Request $request){
+    $topic = Topic::find($id);
+    if (!$topic) return "Not Found";
+    $topic->fill($request->all());
+    $topic = $topic->save();
+    if ($topic) return "Success";
+    return "Error";
+  }
 }
