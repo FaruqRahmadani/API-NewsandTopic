60a917e845ad1da203fd9dae6c4c3e268bdf4516
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-01-20 08:30:44 +0700

    detail parameter method

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,9 +8,18 @@ List Method API
 - Delete News [/news/{id}] (DELETE)
 - Filter News [/news/filter/{ draft / publish / deleted}] (GET)
 
+// NEWS PARAMETER
+- title = REQUIRED, STRING
+- content = REQUIRED, STRING
+- topic_id[] = REQUIRED, INTEGER
+- publish_at = TIMESTAMP
+
 //TOPIC
 - Get All Topic [/topic] (GET)
 - Get Specific Topic [/topic/{id}] (GET)
 - Store Topic [/topic] (POST)
 - Update Topic [/topic/{id}] (PUT)
 - Delete Topic [/topic/{id}] (DELETE)
+
+// TOPIC PARAMETER
+- name = REQUIRED, STRING
